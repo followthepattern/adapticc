c251b6d5be567244f06f6217ba5d18a7c9e0e514
huszkacs <huszkacs@gmail.com>
2022-02-01 23:56:56 +0100

    Adds height calculation

--- apps/frontend/src/layouts/AccountLayout.tsx
+++ apps/frontend/src/layouts/AccountLayout.tsx
@@ -2,10 +2,10 @@ const AccountLayout = (props: any) => {
   return (
     <>
       <div className="h-full flex">
-        <div className="flex-none w-[250px] h-full bg-yellow-200">navbar</div>
+        <div className="w-[250px] h-full bg-yellow-200">navbar</div>
         <div className="grow h-full">
           <div className="w-full h-[50px] bg-red-200">header</div>
-          <div className="grow w-full h-full bg-violet-300">content</div>
+          <div className="h-[calc(100%-50px)] w-full bg-violet-300">{props.children}</div>
         </div>
       </div>
     </>
