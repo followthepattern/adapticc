953d70ffea5a7320ad8879f09109c5e3f2345c82
huszkacs <huszkacs@gmail.com>
2022-02-13 15:06:42 +0100

    Login button formatting

--- apps/frontend/src/modules/login/components/LoginForm.tsx
+++ apps/frontend/src/modules/login/components/LoginForm.tsx
@@ -34,7 +34,9 @@ const LoginForm = () => {
           Password
         </label>
       </div>
-      <Form.Submit className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Login</Form.Submit>
+      <div className="md:flex md:justify-center">
+      <Form.Submit className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm md:w-60 xs:w-full px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Login</Form.Submit>
+      </div>
     </Form>
   );
 };
