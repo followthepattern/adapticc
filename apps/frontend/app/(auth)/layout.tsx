c74ba1da9dc47fafe5604fa35683d2b2e8c7e0f7
huszkacs <huszkacs@gmail.com>
2023-07-30 16:00:57 +0200

    fixes login navigation

--- apps/frontend/app/(auth)/layout.tsx
+++ apps/frontend/app/(auth)/layout.tsx
@@ -2,20 +2,21 @@ import WithGraphQL from "@/components/withGraphQL";
 import { ACCOUNT_HOME } from "@/lib/constants";
 import { useTokenStore } from "@/lib/store";
 import useHasMounted from "@/lib/useMounted";
-import { Outlet, redirect } from "react-router-dom";
+import { Outlet, redirect, useNavigate } from "react-router-dom";
 
 interface AuthLayoutProperties {}
 
 export default function AuthLayout(props: AuthLayoutProperties) {
     const { token } = useTokenStore();
     const hasMounted = useHasMounted();
+    const navigate = useNavigate();
 
     if (!hasMounted) {
         return null;
     }
 
     if (token) {
-        redirect(ACCOUNT_HOME);
+        navigate(ACCOUNT_HOME);
     }
 
     return (
