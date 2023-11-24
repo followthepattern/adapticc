42655a303838ee2a0c60e9f563344b3293d03e59
huszkacs <huszkacs@gmail.com>
2023-11-24 16:55:03 +0800

    Updates login form removes unnecessary formatting

--- apps/frontend/app/(auth)/login/page.tsx
+++ apps/frontend/app/(auth)/login/page.tsx
@@ -12,10 +12,10 @@ export default function Login() {
     }
 
     return (
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="flex flex-col justify-center min-h-full px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Sign in to your account
+                <h2 className="mt-10 text-2xl font-bold text-center text-gray-900">
+                    Sign in to your account!
                 </h2>
             </div>
             <LoginForm />
