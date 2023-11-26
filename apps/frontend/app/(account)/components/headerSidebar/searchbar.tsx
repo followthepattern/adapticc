b1daaf802ae5921a2f6a5fd6b4c64e05d7f86012
huszkacs <huszkacs@gmail.com>
2023-11-26 18:32:53 +0800

    refactors searchbar

--- apps/frontend/app/(account)/components/headerSidebar/searchbar.tsx
+++ apps/frontend/app/(account)/components/headerSidebar/searchbar.tsx
@@ -1,24 +1,19 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import SearchIcon from "@/app/icons/search";
 
 interface SearchbarProperties { }
 
 export default function Searchbar(props: SearchbarProperties) {
     return (
-        <form className="relative flex flex-1" action="#" method="GET">
-            <label htmlFor="search-field" className="sr-only">
-                Search
-            </label>
-            <MagnifyingGlassIcon
-                className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
-                aria-hidden="true"
+        <div className="relative flex flex-auto">
+            <SearchIcon
+                className="absolute left-0 w-5 h-full text-gray-600 pointer-events-none"
             />
             <input
-                id="search-field"
-                className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-                placeholder="Search..."
-                type="search"
+                className="block w-full h-full py-0 pl-10 border-0 placeholder:text-gray-400 focus:ring-0"
+                placeholder="Resources..."
+                type="text"
                 name="search"
             />
-        </form>
+        </div>
     )
 }
