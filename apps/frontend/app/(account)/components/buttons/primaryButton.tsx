b191af165001d320a6960f813b733f161f8bed99
huszkacs <huszkacs@gmail.com>
2023-12-02 18:55:29 +0700

    Primary button fix

--- apps/frontend/app/(account)/components/buttons/primaryButton.tsx
+++ apps/frontend/app/(account)/components/buttons/primaryButton.tsx
@@ -3,7 +3,7 @@ export default function PrimaryButton(props: React.DetailedHTMLProps<React.Butto
         <button
             type="button"
             className="px-4 py-2 font-semibold text-blue-500 border border-blue-500 rounded-lg hover:bg-gray-100 focus:bg-gray-200"
-            {...PushSubscriptionOptions}
+            {...props}
         >
             {props.children}
         </button>
