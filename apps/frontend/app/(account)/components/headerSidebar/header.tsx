c117ae560a4c4370894047002c0ec4b3b1e52e20
huszkacs <huszkacs@gmail.com>
2023-12-03 00:31:45 +0700

    Removes searchbar

--- apps/frontend/app/(account)/components/headerSidebar/header.tsx
+++ apps/frontend/app/(account)/components/headerSidebar/header.tsx
@@ -2,7 +2,6 @@ import { Dispatch, SetStateAction } from 'react'
 import {
     Bars3Icon,
 } from '@heroicons/react/24/outline'
-import Searchbar from './searchbar';
 import ProfileMenu from './profileMenu';
 
 interface HeaderProperties {
@@ -13,25 +12,15 @@ interface HeaderProperties {
 export default function Header(props: HeaderProperties) {
 
     return (
-        <div className="sticky top-0 z-40 flex items-center h-16 px-4 bg-white border-b border-gray-200 shadow-sm shrink-0 gap-x-4 sm:gap-x-6 sm:px-6 lg:px-8">
-            <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => {  props.setSidebarOpen(!props.sidebarOpen) }}>
+        <div className="sticky top-0 z-40 flex items-center h-16 px-6 bg-white border-b border-gray-200 shadow-sm shrink-0 gap-x-4 sm:gap-x-6 sm:px-6 lg:px-8">
+            <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => { props.setSidebarOpen(!props.sidebarOpen) }}>
                 <span className="sr-only">Open sidebar</span>
                 <Bars3Icon className="w-6 h-6" aria-hidden="true" />
             </button>
 
-            {/* Separator */}
-            <div className="w-px h-6 bg-gray-900/10 lg:hidden" aria-hidden="true" />
-
-            <div className="flex self-stretch flex-1 gap-x-4 lg:gap-x-6">
-                <Searchbar />
-                <div className="flex items-center gap-x-4 lg:gap-x-6">
-
-                    {/* Separator */}
-                    <div className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10" aria-hidden="true" />
-
-                    {/* Profile dropdown */}
-                    <ProfileMenu />
-                </div>
+            <div className="flex justify-end w-full gap-x-4">
+                <div className="flex w-px h-6 lg:bg-gray-400" aria-hidden="true" />
+                <ProfileMenu />
             </div>
         </div>
     )
