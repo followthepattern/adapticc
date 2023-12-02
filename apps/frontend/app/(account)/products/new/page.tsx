991f1acb67bba6f8f4542dad97bf6d1d57fb15fc
huszkacs <huszkacs@gmail.com>
2023-12-02 22:54:30 +0700

    Refactors new product page

--- apps/frontend/app/(account)/products/new/page.tsx
+++ apps/frontend/app/(account)/products/new/page.tsx
@@ -4,6 +4,13 @@ import { useNavigate } from "react-router-dom";
 import { RESOURCE_URL } from "../page";
 import { useEffect } from "react";
 import { Product } from "@/models/product";
+import SingleLayout from "../../components/singlePage/layout";
+import GridFields from "../../components/gridFields/gridFields";
+import Label from "../../components/labels/label";
+import Input from "../../components/inputs/input";
+import TextArea from "../../components/inputs/textarea";
+import SecondaryButton from "../../components/buttons/secondaryButton";
+import PrimaryButton from "../../components/buttons/primaryButton";
 
 export default function ProductNew() {
     const [executeMutation, { createLoading, createError, createResult }] = useCreateProduct();
@@ -34,56 +41,35 @@ export default function ProductNew() {
     }
 
     return (
-        <form action="">
-            <div className="space-y-12">
-                <div className="border-b border-gray-900/10 pb-12">
-                    <h2 className="text-base font-semibold leading-7 text-gray-900">New Product</h2>
-
-                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div className="sm:col-span-3">
-                            <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900">
-                                Title
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    {...register("title")}
-                                    type="text"
-                                    id="title"
-                                    defaultValue=""
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-
-                        <div className="col-span-full">
-                            <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">
-                                Description
-                            </label>
-                            <div className="mt-2">
-                                <textarea
-                                    id="description"
-                                    {...register("description")}
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    defaultValue=""
-                                />
-                            </div>
-                        </div>
+        <SingleLayout>
+            <SingleLayout.Title>New Product</SingleLayout.Title>
+            <form>
+                <GridFields className="py-6">
+                    <div className="sm:col-span-2">
+                        <Label htmlFor="title">
+                            Title
+                        </Label>
+                        <Input
+                            {...register("title")}
+                            type="text"
+                            id="title"
+                        />
                     </div>
-                </div>
-            </div>
-
-            <div className="mt-6 flex items-center justify-end gap-x-6">
-                <button type="button" className="text-sm font-semibold leading-6 text-gray-900" onClick={onCancel}>
-                    Cancel
-                </button>
-                <button
-                    type="button"
-                    onClick={onCreate}
-                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                    Create
-                </button>
-            </div>
-        </form>
+                    <div className="sm:col-span-2">
+                        <Label htmlFor="description">
+                            Description
+                        </Label>
+                        <TextArea
+                            id="description"
+                            {...register("description")}
+                        />
+                    </div>
+                </GridFields>
+                <SingleLayout.Footer className="justify-end">
+                    <SecondaryButton onClick={onCancel}>Cancel</SecondaryButton>
+                    <PrimaryButton onClick={onCreate}>Create</PrimaryButton>
+                </SingleLayout.Footer>
+            </form>
+        </SingleLayout>
     )
 }
