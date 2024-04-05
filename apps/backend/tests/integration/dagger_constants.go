d2ac9e97526d6805682e7e498949b24e7ccdb0cc
Csaba Huszka <huszkacs@gmail.com>
2024-04-05 21:53:34 +0800

    Fixes dagger testcases (#13)
    * Fixes dagger testcases
    
    * Updates golang image tag for dagger integration tests

--- apps/backend/tests/integration/dagger_constants.go
+++ apps/backend/tests/integration/dagger_constants.go
@@ -1,7 +1,8 @@
 package test_integration
 
 const (
-	GolangImage   = "golang:latest"
+	GolangImage   = "golang:1.22"
 	PostgresImage = "postgres:latest"
 	CerbosImage   = "ghcr.io/cerbos/cerbos:latest"
+	AlpineImage   = "alpine:3.19.1"
 )
