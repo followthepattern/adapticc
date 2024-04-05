87da79b7620af7702de7e89d83d96a7ff2166d56
huszkacs <huszkacs@gmail.com>
2024-04-05 21:52:23 +0800

    Updates golang image for adapticc

--- apps/backend/tests/integration/dagger_constants.go
+++ apps/backend/tests/integration/dagger_constants.go
@@ -1,7 +1,7 @@
 package test_integration
 
 const (
-	GolangImage   = "golang:1.21"
+	GolangImage   = "golang:1.22"
 	PostgresImage = "postgres:latest"
 	CerbosImage   = "ghcr.io/cerbos/cerbos:latest"
 	AlpineImage   = "alpine:3.19.1"
